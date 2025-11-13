e0d9392d77e6bfbbf768b6205312b258d4deb291
clementmw <clementmacharia62@gmail.com>
2025-11-14 02:15:35 +0300

    Refactor admin.py to register models and update site titles; update requirements.txt to include new dependencies

--- bank/auth_service/admin.py
+++ bank/auth_service/admin.py
@@ -2,3 +2,30 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.site_header = "EVERGREEN DASHBOARD "
+admin.site.site_title = "EVERGREEN"
+admin.site.index_title = "Welcome to Your your Dashboard"
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in User._meta.fields]
+
+
+@admin.register(EmployeeProfile)
+class EmployeeProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in EmployeeProfile._meta.fields]
+
+@admin.register(CustomerProfile)
+class CustomerProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CustomerProfile._meta.fields]
+
+@admin.register(SessionLogs)
+class SessionLogsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in SessionLogs._meta.fields]
+@admin.register(KycProfile)
+class ApplicantKYCAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in KycProfile._meta.fields]
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Role._meta.fields]
