e7efaa98f1dfe78bf0f91add87ef80cc00ea697f
clementmw <clementmacharia62@gmail.com>
2025-11-30 23:57:09 +0300

    upted dmin for accounts

--- bank/accounts/admin.py
+++ bank/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
+from .utility import generate_account_number
 
-# Register your models here.
+
+@admin.register(AccountType)
+class AccountTypeAdmin(admin.ModelAdmin):
+    list_display =[field.name for field in AccountType._meta.fields]
+    search_fields = ("code","name")
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display =[field.name for field in Account._meta.fields]
+    search_fields = ("account_number","customer")
+
+    def save(self, *args, **kwargs):
+        if not self.account_number:
+            self.account_number = generate_account_number()
+            super().save(*args, **kwargs)
