113cafd2f2f0d49c7c19f41f9075d2f93764f73d
clementmw <clementmacharia62@gmail.com>
2026-01-09 19:10:20 +0300

    feat(settings): Add STATIC_ROOT and MEDIA_ROOT settings for static and media files management
    feat(admin): Enhance TransactionAdmin and TransactionLimitAdmin for better admin interface
    
    feat(utility): Implement custom CursorPagination class for efficient transaction batching
    
    feat(urls): Ensure transaction history URL path is correctly defined

--- bank/transactions/admin.py
+++ bank/transactions/admin.py
@@ -7,3 +7,16 @@ class FeeRuleAdmin(admin.ModelAdmin):
     list_display = ('transaction_type', 'min_amount', 'max_amount', 'fee_amount')
     list_filter = ('transaction_type',)
     search_fields = ('transaction_type',)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('transaction_type', 'amount', 'fee', 'created_at')
+    list_filter = ('transaction_type',)
+    search_fields = ('transaction_type',)
+
+@admin.register(TransactionLimit)
+class TransactionLimitAdmin(admin.ModelAdmin):
+    list_display =[field.name for field in TransactionLimit._meta.fields]
+
+    
